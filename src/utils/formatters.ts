299005ff0509b850256e3782491b16d0f95e1bdd
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2026-02-01 23:00:13 +0700

    fix error type description at formatter (generatePlaceholderCard)

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,35 +1,38 @@
-import { Card } from "@/types/board.type"
+import { Card } from "@/types/board.type";
 
-export function generatePlaceholdeCard(column: any):Card {
-    return {
-        _id: column._id+'-placeholder-card',
-        label: '',
-        FE_placeholderCard: true,
-        columnId: column._id,
-        checklist:[],
-        status: false,
-        cover: "",
-        description: "",
-        attachments: [],
-        date: {
-            startDate: null,
-            dueDate: null,
-            recurring: "Daily",
-            reminder: "None"
-        },
-        tag: [],
-        joined: []
-        }
+export function generatePlaceholdeCard(column: any): Card {
+  return {
+    _id: column._id + "-placeholder-card",
+    label: "",
+    FE_placeholderCard: true,
+    columnId: column._id,
+    checklist: [],
+    status: false,
+    cover: "",
+    description: {
+      time: null,
+      blocks: [],
+    },
+    attachments: [],
+    date: {
+      startDate: null,
+      dueDate: null,
+      recurring: "Daily",
+      reminder: "None",
+    },
+    tag: [],
+    joined: [],
+  };
 }
 
 export function slugtify(val: string) {
-    if (!val) return ''
-    return val
-    .normalize('NFKD')
-    .replace(/[\u0300-\u036f]/g, '')
+  if (!val) return "";
+  return val
+    .normalize("NFKD")
+    .replace(/[\u0300-\u036f]/g, "")
     .trim()
     .toLowerCase()
-    .replace(/[^a-z0-9 -]/g, '')
-    .replace(/\s+/g,'-')
-    .replace(/-+/g,'-')
+    .replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
 }
